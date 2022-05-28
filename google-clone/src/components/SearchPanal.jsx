b39969d70d7efd10efa93254fb4379db518938a8
adadesions <adadesions@gmail.com>
2022-05-28 14:33:48 +0700

    Add hover style to text-search-box

--- google-clone/src/components/SearchPanal.jsx
+++ google-clone/src/components/SearchPanal.jsx
@@ -1,14 +1,20 @@
-import SearchBtn from "../elements/SearchBtn"
+import SearchBtn from "../elements/SearchBtn";
 
 const SearchPanal = () => {
-
   return (
-    <div className="search-panal">
-      <SearchBtn label="Google Search"/>
-      <SearchBtn label="I'm feeling Lucky"/>
+    <div className="container">
+      <div className="search-panal">
+        <SearchBtn label="Google Search" />
+        <SearchBtn label="I'm feeling Lucky" />
+      </div>
+      <div className="search-panal-text">
+        <span className="subtext">
+          Google offered in:
+          <a className="lang-link" href="/">ภาษาไทย</a>
+        </span>
+      </div>
     </div>
-  )
+  );
+};
 
-}
-
-export default SearchPanal
+export default SearchPanal;
